db49abcd21736e888f43e09bef0e846c9a369610
Suraj Prasad-13 <spyadav0413@gmail.com>
2025-10-08 10:11:56 +0530

    update authContext

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,29 +1,4 @@
 import { createContext, useContext, useEffect, useState } from "react";
-
-// const AuthContext = createContext(null);
-
-// export const AuthProvider = ({ children }) => {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const [isLoading, setisLoading] = useState(true);
-//   useEffect(() => {
-//     const access = localStorage.getItem("authAccess");
-//     if (access) {
-//       setIsLoggedIn(true);
-//     }
-//     setisLoading(false);
-//   }, []);
-
-//   return (
-//     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, isLoading }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export const useAuth = () => useContext(AuthContext);
-
-
-
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
